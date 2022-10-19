bd0ff9eb894bfaead77bdb0d0aa2fd27cf12026c
ahmedhridoy371 <40537458+ahmedhridoy371@users.noreply.github.com>
2022-10-19 14:02:23 +0600

    T

--- Android.mk
+++ Android.mk
@@ -7,3 +7,13 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(TARGET_DEVICE),whyred)
 
 endif
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libtime_genoff
+LOCAL_MODULE_OWNER := xiaomi
+LOCAL_SRC_FILES := proprietary/vendor/lib/libtime_genoff.so
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+include $(BUILD_PREBUILT)
